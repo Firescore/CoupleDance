1bf65c0d12ccb58c386d4754d3fdc6c4fabb38d3
virenraghvani <virenraghvani@users.noreply.github.com>
2021-03-19 12:51:44 +0530

    Initial commit

---
+++ Assets/_coupleDance/_scripts/gameManager_script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager_script : MonoBehaviour
+{
+    public void Dance1_func()
+    {
+        print("dance 11");
+    }
+
+    public void Dance2_func()
+    {
+        print("dance 22");
+    }
+
+    public void Dance3_func()
+    {
+        print("dance 33");
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# proj12_coupleDance
+ 
